1ac60dab13ecd02cd178b971c31491eb8f820744
kalai <kalai@cdp360.com>
2024-02-28 11:00:11 +0530

    new folder structures push

--- src/Routing/Routers.js
+++ src/Routing/Routers.js
@@ -1,24 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
-
-
-
-
-
 import React from 'react'
-import Sigin from '../components/auth/Sigin';
-import Signup from '../components/auth/Signup';
-import Home from '../components/home/Home';
-
+import { Home } from '../pages';
 function Routers() {
     return (
         <div>
             <Routes>
                 <Route exact path="/" element={<Home />}></Route>
-                <Route path="/register" element={<Signup />}></Route>
-                <Route path="/login" element={<Sigin />}></Route>
             </Routes>
         </div>
     )
 }
-
 export default Routers
